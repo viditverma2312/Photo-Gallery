5830c4c28bba0303adff57a4d4681fb707a6dd21
viditverma2312 <viditverma2312@gmail.com>
2021-06-07 10:45:31 +0530

    updated

--- src/comps/ProgressBar.js
+++ src/comps/ProgressBar.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import useStorage from '../hooks/useStorage';
+import { motion } from 'framer-motion';
 
 const ProgressBar = ({ file, setFile }) => {
   const { url, progress } = useStorage(file);
-  console.log(progress, url);
+  
+  useEffect(() => {
+    if (url) {
+      setFile(null);
+    }
+  }, [url, setFile])
 
   return (
-    <div className="progress-bar">Progress</div>
+    <motion.div className="progress-bar"
+      initial={{ width: 0 }}
+      animate={{ width: progress + '%' }}
+    ></motion.div>
   )
 
 }
